7502c2d56e8819009736bf33bc1406cefb53a237
Ryan Peters <ryanpeters@microsoft.com>
2024-03-28 16:46:43 -0400

    added remaining components

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Security;
+using WcfService;
+
+Console.WriteLine("Waiting 2 seconds for service to become available...\n");
+Thread.Sleep(2000);
+
+var basicHttpBinding = new BasicHttpBinding(BasicHttpSecurityMode.Transport);
+
+var endpoint = new EndpointAddress("https://127.0.0.1:7276/service.svc");
+
+var factory = new ChannelFactory<IServiceChannel>(basicHttpBinding, endpoint);
+
+factory.Credentials.ServiceCertificate.SslCertificateAuthentication = new X509ServiceCertificateAuthentication()
+{
+    CertificateValidationMode = X509CertificateValidationMode.None,
+    RevocationMode = X509RevocationMode.NoCheck
+};
+
+var channel = factory.CreateChannel();
+
+var httpRequestProperty = new HttpRequestMessageProperty();
+
+// JWT token
+var token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwicG9zaXRpb24iOiJ0ZWFjaGVyIiwiaWF0IjoxNTE2MjM5MDIyfQ.YT4H1tnwk0HnLiBUxuLpSMbSYPQD2OdaX2cvnYDwujE";
+
+httpRequestProperty.Headers[HttpRequestHeader.Authorization] = $"Bearer {token}";
+
+OperationContext.Current = new OperationContext(channel);
+OperationContext.Current.OutgoingMessageProperties[HttpRequestMessageProperty.Name] = httpRequestProperty;
+
+var result = channel.GetData(3);
+
+Console.WriteLine(result);
--- WcfService/Program.cs
+++ WcfService/Program.cs
@@ -1,17 +1,71 @@
-﻿var builder = WebApplication.CreateBuilder();
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder();
 
 builder.Services.AddServiceModelServices();
 builder.Services.AddServiceModelMetadata();
 builder.Services.AddSingleton<IServiceBehavior, UseRequestHeadersForMetadataAddressBehavior>();
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    var issuerSigningKey = builder.Configuration["IssuerSigningKey"];
+
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateAudience = false,
+        ValidateIssuer = false,
+        RequireExpirationTime = false,
+        //ValidateActor = false,
+        //ValidateLifetime = true,
+
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(issuerSigningKey))
+    };
+});
+
+builder.Services.AddAuthorization(options =>
+{
+    options.DefaultPolicy = new AuthorizationPolicyBuilder()
+        .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme)
+        .RequireAuthenticatedUser()
+        .RequireClaim("name")
+        .RequireClaim("position")
+        .RequireAssertion(context => context.User.FindFirstValue("position") == "teacher")
+        .Build();
+
+    //options.AddPolicy(JwtBearerDefaults.AuthenticationScheme, policy =>
+    //{
+    //    policy.AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme);
+    //    policy.RequireClaim(ClaimTypes.Name);
+    //    policy.RequireAuthenticatedUser();
+
+    //    policy.RequireAssertion(context =>
+    //    {
+    //        var user = context.User.Claims;
+
+    //        return false;
+    //    });
+    //});
+});
+
 var app = builder.Build();
 
 app.UseServiceModel(serviceBuilder =>
 {
+    var basicHttpBinding = new BasicHttpBinding(BasicHttpSecurityMode.Transport);
+    basicHttpBinding.Security.Transport.ClientCredentialType = HttpClientCredentialType.InheritedFromHost;
+
     serviceBuilder.AddService<Service>();
-    serviceBuilder.AddServiceEndpoint<Service, IService>(new BasicHttpBinding(BasicHttpSecurityMode.Transport), "/Service.svc");
+    serviceBuilder.AddServiceEndpoint<Service, IService>(basicHttpBinding, "/Service.svc");
+
     var serviceMetadataBehavior = app.Services.GetRequiredService<ServiceMetadataBehavior>();
     serviceMetadataBehavior.HttpsGetEnabled = true;
 });
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.Run();
