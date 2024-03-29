ff2c7eecda5ef6d67a2bc9670bceb4465e1742bd
Ryan Peters <ryanpeters@microsoft.com>
2024-03-29 10:48:48 -0400

    remove unused usings

--- WcfService/Program.cs
+++ WcfService/Program.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.IdentityModel.Tokens;
-using System.Security.Claims;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder();
